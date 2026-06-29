d1d59775815789b92e7647065264ea32845de8d2
oubahi-hossin <sisooka2020@gmail.com>
2026-06-29 22:16:21 +0100

    Fix deployment path

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react' // أو المكون الذي تستخدمه
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      }
-    }
-  }
+  plugins: [react()],
+  base: '/Happy-birthday-Khadijaa-/', // يجب أن تطابق اسم المستودع بالضبط
 })
